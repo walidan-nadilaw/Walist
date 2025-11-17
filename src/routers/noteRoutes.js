0314fca33e2b05e64b399f8bbcf85fed1655dac6
walidan-nadilaw <sirsnakeson@gmail.com>
2025-11-17 19:54:33 +0700

    fix(noteRoute) : add semicolon at the end of line

--- src/routers/noteRoutes.js
+++ src/routers/noteRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getAllNotes, getOneNote, createNote, updateNote, deleteNote} from "../controllers/noteControllers.js"
+import {getAllNotes, getOneNote, createNote, updateNote, deleteNote} from "../controllers/noteControllers.js";
 
 const router = express.Router();
 
@@ -8,5 +8,5 @@ router.get("/:id", getOneNote)
 router.post("/", createNote);
 router.put("/:id", updateNote)
 router.delete("/:id", deleteNote)
-
+    
 export default router
