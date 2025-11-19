a623b257f4862c18623e34466d6e46b499929f7c
walidan-nadilaw <sirsnakeson@gmail.com>
2025-11-19 12:39:53 +0700

    feat : improve controllers

--- src/controllers/noteControllers.js
+++ src/controllers/noteControllers.js
@@ -1,13 +1,33 @@
+import Note from "../models/Note.js"
+
 export async function getAllNotes(req,res) {
-    res.status(200).send("You just fetched 20 notes");
+    try{
+        const AllNote= await Note.find(); //fetch all notes from database
+        res.status(200).json(AllNote);//send back all notes as JSON response with status 200
+    }catch(error){
+        console.error("Error get All Notes in noteController: getAllNotes", error);
+        res.status(500).json({message: "Internal Server Error"}) //send back error message with status 500
+    }
 }
 
-export async function getOneNote(req,res) {
+export async function getOneNote(req,res) { //req.params.id gets the id from the request URL
     res.status(200).send(`you got note number ${req.params.id}`);
 }
 
 export async function createNote(req,res) {
-    res.status(201).json({message: "note successfully created"});
+    try {
+        const {title, content} = req.body; //destructure title and content from request body (see notes on javaScript json destructuring)
+        const newNote = new Note ({title, content});
+        await newNote.save(); //save the new note to the database
+        res.status(201).json(
+            {
+                message: "note successfully created",
+                note: newNote
+            });
+    } catch(error){
+        console.error("Error creating a note in noteController: createNote", error);
+        res.status(500).json({message: "Internal Server Error"})
+    }
 }
 
 export async function updateNote(req,res) {
