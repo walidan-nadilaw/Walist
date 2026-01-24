9feb9adfcd63fae4ea1df15e6a0485debbab6f33
walidan-nadilaw <sirsnakeson@gmail.com>
2026-01-24 22:28:12 +0700

    feat : config deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //import express framework for web server
 import dotenv from "dotenv"; //import dotenv to manage env
 import cors from "cors";
+import path from "path";
 
 import noteRoutes from "../src/routers/noteRoutes.js"; //import note routes for handling /api/notes requests
 import connectDB from "../src/config/db.js"; //import custom function to connect to database
@@ -10,15 +11,26 @@ dotenv.config(); //load environment variables from .env file
 
 const app = express(); //make a new express app for web server and store it as app
 const PORT = process.env.PORT || 5001; //get port from env or default to 5001
+const __dirname = path.resolve();
 
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-        origin: "http://localhost:5173",
+      origin: "http://localhost:5173",
     })
-);  
+  );
+}
 app.use(express.json()); //middleware to parse incoming JSON requests
 app.use(ratelimiter); //apply ratelimiter middleware to all incoming requests
 app.use("/api/notes", noteRoutes); //for any request to /api/notes, access route that matches in noteRoutes
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then( ()=>{ //connect to database first before starting server
     app.listen(PORT, () => { //start server and listen for request on specified PORT
