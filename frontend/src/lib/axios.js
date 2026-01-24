35eb4804d24c91ddb6d466032c5b930ef1aaa1f2
walidan-nadilaw <sirsnakeson@gmail.com>
2026-01-24 21:17:43 +0700

    feat: config for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASEURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASEURL,
 });
 
 export default api;
