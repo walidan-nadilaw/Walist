3f341b338ddd605a3f7e2ef072fbb08c705bc4ec
walidan-nadilaw <sirsnakeson@gmail.com>
2026-01-25 09:39:30 +0700

    feat : ui update
    make the dot more glowy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="relative h-full w-full bg-base-300">
       <div className="absolute h-full w-full pointer-events-none
-        bg-[radial-gradient(#e5e7eb40_1px,transparent_1px)] 
+        bg-[radial-gradient(#e5e7eb80_1px,transparent_1px)] 
         [background-size:48px_48px] 
         [mask-image:radial-gradient(ellipse_60%_60%_at_50%_50%,#000_70%,transparent_100%)]"   
       ></div>
